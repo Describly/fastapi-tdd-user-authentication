81bd3839fe3734218034fea042cf8f9d41244e7d
Keshari Nandan <keshari.nandan2@gmail.com>
2023-10-06 13:31:12 +0530

    Added main.py

---
+++ app/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+
+def create_application():
+    application = FastAPI()
+    return application
+
+
+app = create_application()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hi, I am Describly. Awesome - Your setrup is done & working."}
